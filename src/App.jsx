ba1b911b65fdce1dc0ade430086fab935bf683fc
david-damo <sanjeev_jha77@rediffmail.com>
2026-03-28 06:50:31 +0530

    Again Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Dashboard from "./Dashboard";   // ✅ keep this
+import Dashboard from "./Dashboard";
 import Admin from "./Admin";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
 
