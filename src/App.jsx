ddcdac169202733498a92befff4e1c94300250a5
david-damo <sanjeev_jha77@rediffmail.com>
2026-03-28 13:46:47 +0530

    Update for router fix in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import { useState } from "react";
@@ -7,14 +7,15 @@ function App() {
   const [auth, setAuth] = useState(localStorage.getItem("auth"));
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/admin" element={
-          auth ? <Dashboard isAdmin={true}/> : <Login setAuth={setAuth}/>
-        } />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route
+        path="/admin"
+        element={
+          auth ? <Dashboard isAdmin={true} /> : <Login setAuth={setAuth} />
+        }
+      />
+    </Routes>
   );
 }
 
