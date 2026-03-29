565566cf1242d5f2c9f3b1536c327f679f654e38
unknown <sanjeev_jha77@rediffmail.com>
2026-03-29 13:28:29 +0530

    Updated App.jsx -removed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 
 function App() {
   return (
-    <Router>
+    <div>
+      <nav>
+        <Link to="/">Dashboard</Link> |{" "}
+        <Link to="/admin">Admin</Link>
+      </nav>
+
       <Routes>
-        <Route path="/" element={<Dashboard isAdmin={false} />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/admin" element={<Login />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
